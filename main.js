4f32eb610ff6bf0eea700ebabb7e9aa1122b63cf
Ale Mariles <97320635+marilesmartineza@users.noreply.github.com>
2023-07-28 16:55:43 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+difference=0;
+rightWristX=0;
+leftWristX=0;
+
+    function setup(){
+        video=createCapture(VIDEO);
+        video.size(300,300);
+        video.position(650,150);
+        canvas=createCanvas(300,300);
+        canvas.position(300,150);
+        poseNet=ml5.poseNet(video,modelLoaded);
+        poseNet.on('pose',gotPoses);
+            //(x,y) X=ancho, Y=altura//
+    }
+
+    function modelLoaded(){
+        console.log("¡Modelo Cargado! :D");
+    }
+
+    function gotPoses(results){
+        if(results.length > 0){
+            console.log(results);
+            
+            leftWristX = results[0].pose.leftWrist.x;
+            rightWristX = results[0].pose.rightWrist.x;
+            difference = floor(leftWristX - rightWristX);
+
+            console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);    
+        }
+    }
+
+    function draw(){
+        background("rgb(92, 191, 198)");
+        textSize(difference);
+        document.getElementById("square_side").innerHTML= "El tamaño de fuente es: " + difference + "px";
+        fill("rgb(20, 123, 110)");
+        stroke("rgb(20, 123, 110)");
+        text("Que difícil es esto de programar ಥ⁠‿⁠ಥ", 10, 10, difference);
+    }  
