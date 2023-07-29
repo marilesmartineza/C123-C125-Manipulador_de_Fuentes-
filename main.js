385a61f5d6aa8724b036d80854a9aaab1b4b2cbb
Ale Mariles <97320635+marilesmartineza@users.noreply.github.com>
2023-07-28 19:30:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ leftWristX=0;
     function setup(){
         video=createCapture(VIDEO);
         video.size(300,300);
-        video.position(650,150);
-        canvas=createCanvas(300,300);
-        canvas.position(300,150);
+        video.position(800,150);
+        canvas=createCanvas(500,300);
+        canvas.position(100,150);
         poseNet=ml5.poseNet(video,modelLoaded);
         poseNet.on('pose',gotPoses);
             //(x,y) X=ancho, Y=altura//
@@ -37,3 +37,8 @@ leftWristX=0;
         stroke("rgb(20, 123, 110)");
         text("Que difícil es esto de programar ಥ⁠‿⁠ಥ", 10, 10, difference);
     }  
+
+
+    function prueba() {
+        window.alert("Hola criatura (⁠｡⁠•̀⁠ᴗ⁠-⁠)⁠✧ Esto salió de puro milagro (⁠・⁠-⁠・⁠;⁠)");
+    }
